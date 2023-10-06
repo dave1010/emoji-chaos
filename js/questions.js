898348879818f1e94d22e2b9afe9a36b433cb271
Dave Hulbert <dave1010@gmail.com>
2023-10-06 14:15:21 +0100

    People questionss

--- js/questions.js
+++ js/questions.js
@@ -188,14 +188,14 @@
 
 
 var questions = [
-    {q: "Q1. What is 1 + 1?", a: "2", no: ["1", "7", "3.141"]},
+    {q: "Q1. What is 1 + 1?", a: "2", no: ["1", "7", "3.14"]},
     {q: "Q2. What is the capital of France?", a: "Paris", no: ["London", "Berlin", "Madrid"]},
-    {q: "Q3. Who wrote 'To Kill a Mockingbird'?", a: "Harper Lee", no: ["J.K. Rowling", "George Orwell", "Jane Austen"]},
+    {q: "Q3. Where is Terry?", a: "Devon", no: ["Dorset", "Hamshire", "Somerset"]},
     {q: "Q4. What is the chemical symbol for gold?", a: "Au", no: ["Ag", "Ga", "Ge"]},
     {q: "Q5. Which planet is known as the Red Planet?", a: "Mars", no: ["Venus", "Jupiter", "Saturn"]},
     {q: "Q6. Who painted the Mona Lisa?", a: "Leonardo da Vinci", no: ["Vincent van Gogh", "Pablo Picasso", "Michelangelo"]},
     {q: "Q7. What is the largest mammal?", a: "Blue Whale", no: ["Elephant", "Giraffe", "Orca"]},
-    {q: "Q8. What is the smallest prime number?", a: "2", no: ["1", "0", "-1"]},
+    {q: "Q8. Where is Jennifer?", a: "Hampshire", no: ["Dorset", "Devon", "Somerset"]},
     {q: "Q9. Who is the author of '1984'?", a: "George Orwell", no: ["Aldous Huxley", "J.R.R. Tolkien", "Ernest Hemingway"]},
     {q: "Q10. Which element is represented by the letter 'K'?", a: "Potassium", no: ["Krypton", "Kryptonite", "Kettle"]}
 ];
