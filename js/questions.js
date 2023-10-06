4d87d2220d33dfe9dbdcb76dc1ae8ca5597f7a08
Dave Hulbert <dave1010@gmail.com>
2023-10-06 14:56:42 +0100

    Questions

--- js/questions.js
+++ js/questions.js
@@ -197,7 +197,24 @@ var questions = [
     {q: "Q7. What is the largest mammal?", a: "Blue Whale", no: ["Elephant", "Giraffe", "Orca"]},
     {q: "Q8. Where is Jennifer?", a: "Hampshire", no: ["Dorset", "Devon", "Somerset"]},
     {q: "Q9. Who is the author of '1984'?", a: "George Orwell", no: ["Aldous Huxley", "J.R.R. Tolkien", "Ernest Hemingway"]},
-    {q: "Q10. Which element is represented by the letter 'K'?", a: "Potassium", no: ["Krypton", "Kryptonite", "Kettle"]}
+    {q: "Q10. Which element is represented by the letter 'K'?", a: "Potassium", no: ["Krypton", "Kryptonite", "Kettle"]},
+
+    {q: "Q11. What is the code name of our mobile ticketing API?", a: "Puffin", no: ["Sparrow", "Falcon", "Hawk"]},
+    {q: "Q12. How do you pronounce (Tom) Quay?", a: "Kway", no: ["Key", "Cheee", "Kwaa-aye"]},
+    {q: "Q13. What animal can't jump?", a: "Elephant", no: ["Frog", "Kangaroo", "Rabbit"]},
+    {q: "Q14. How much is the bus fare cap?", a: "£2", no: ["£2.50", "£10", "£3"]},
+    {q: "Q15. What is the currency of Japan?", a: "Yen", no: ["Won", "Yuan", "Rupee"]},
+    {q: "Q16. What is the primary language spoken in Brazil?", a: "Portuguese", no: ["Spanish", "English", "French"]},
+    {q: "Q17. What is the most common gas in the Earth's atmosphere?", a: "Nitrogen", no: ["Oxygen", "Carbon Dioxide", "Hydrogen"]},
+    {q: "Q18. What does HT in HTTP stand for?", a: "HyperText", no: ["HighTech", "HyperTab", "HiTransfer"]},
+    {q: "Q19. What is the powerhouse of the cell?", a: "Mitochondria", no: ["Nucleus", "Ribosome", "Endoplasmic Reticulum"]},
+    {q: "Q20. What is the main ingredient in traditional Japanese Sushi?", a: "Rice", no: ["Fish", "Seaweed", "Soy Sauce"]},
+    {q: "Q21. Who cut off his own ear?", a: "Van Gogh", no: ["Picasso", "Da Vinci", "Rembrandt"]},
+    {q: "Q22. What's the shortest war in history?", a: "38 min", no: ["1 day", "7 hr", "6 days"]},
+    {q: "Q23. What's the national animal of Scotland?", a: "Unicorn", no: ["Lion", "Eagle", "Wales"]},
+    {q: "Q24. What is the main ingredient in guacamole?", a: "Avocado", no: ["Tomato", "Onion", "Lime"]},
+    {q: "Q25. Who is known as the 'Father of Computer Science'?", a: "Alan Turing", no: ["Bill Gates", "Steve Jobs", "Charles Babbage"]}
+
 ];
 
 
